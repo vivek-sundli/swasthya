bf89e3065472fc0df86a8d074d884dbf0191a22b
vivek-sundli <viveksundli@gmail.com>
2024-04-15 23:16:17 +0530

    changing website names

--- popup.js
+++ popup.js
@@ -10,17 +10,18 @@ document.addEventListener('DOMContentLoaded', function() {
                     const div = document.createElement('div');
                     const favicon = document.createElement('img');
                     const text = document.createElement('span');
-
+                    
                     const totalSeconds = Math.floor(totalTime / 1000);
                     const minutes = Math.floor(totalSeconds / 60);
                     const seconds = totalSeconds % 60;
+                    let trimmedDomain = domain.replace(/^www\./, '').split('.')[0];
 
                     favicon.src = `https://www.google.com/s2/favicons?domain=${domain}`;
                     favicon.style.height = '16px';
                     favicon.style.width = '16px';
                     favicon.style.marginRight = '8px';
 
-                    text.textContent = `${domain}: ${minutes}:${seconds.toString().padStart(2, '0')}`;
+                    text.textContent = `${trimmedDomain}: ${minutes}:${seconds.toString().padStart(2, '0')}`;
 
                     div.appendChild(favicon);
                     div.appendChild(text);
